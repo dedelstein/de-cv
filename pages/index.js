e3a6a7c9daa45c6fb6b03f5ad26bed145d4cb405
Dan Edelstein <dan.edelstein@aalto.fi>
2023-10-27 00:32:34 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Dan Edelstein's CV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <href src='/public/Tech_Resume.pdf'>CV</href>
+        <h1> <Link href="/CV.pdf">CV</Link></h1>
       </main>
 
       <Footer />
